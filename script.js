5b2739e6b05ca4b2677267933838c31882a533c5
Sheri Nawaz <sherinawaz495@gmail.com>
2025-07-24 17:13:29 +0100

    added variable grid size

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
+const container = document.querySelector(".container");
 function generateGrid(gridSize){
     for(let i = 0; i < gridSize**2; i++){
-        const container = document.querySelector(".container");
         const newDiv = document.createElement('div');
         newDiv.classList.add("content");
         newDiv.style.width = `${100 / gridSize}%`;
@@ -14,4 +13,28 @@ function generateGrid(gridSize){
     }
 }
 
-generateGrid(4)
+generateGrid(16);
+
+function clearGrid(){
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+const btn = document.querySelector("#btn");
+
+btn.onclick = () => {
+  let gridSize;
+  do {
+    const input = prompt("Enter a grid size between 1 and 100:");
+    gridSize = Number(input);
+  } while (
+    isNaN(gridSize) ||        
+    gridSize < 1 ||         
+    gridSize > 100 ||       
+    !Number.isInteger(gridSize) 
+  );
+  clearGrid();
+  generateGrid(gridSize)
+};
+
