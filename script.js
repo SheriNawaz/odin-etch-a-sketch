aa4fc4c84a022436bb0efc653a6cb756118185d0
Sheri Nawaz <sherinawaz495@gmail.com>
2025-07-24 17:02:10 +0100

    Basic iteration

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-for(let i = 0; i < 16*16; i++){
-    const container = document.querySelector(".container");
-    const newDiv = document.createElement('div');
-    newDiv.classList.add("content");
-    container.appendChild(newDiv);
+
+function generateGrid(gridSize){
+    for(let i = 0; i < gridSize**2; i++){
+        const container = document.querySelector(".container");
+        const newDiv = document.createElement('div');
+        newDiv.classList.add("content");
+        newDiv.style.width = `${100 / gridSize}%`;
+        newDiv.style.height = `${100 / gridSize}%`;
+        newDiv.addEventListener('mouseenter', function () {
+            this.classList.add("hover");
+            this.style.backgroundColor = 'black';
+        });
+        container.appendChild(newDiv);
+    }
 }
+
+generateGrid(4)
